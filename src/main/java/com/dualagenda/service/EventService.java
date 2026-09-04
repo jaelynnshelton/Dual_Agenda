a5cd4a18a30165c4022511d9e2395944f0efbab2
Jaelynn Shelton <116772537+Jaelynnshelton@users.noreply.github.com>
2026-09-04 03:27:32 -0400

    Add menu with remove and sort-by-date options

--- src/main/java/com/dualagenda/service/EventService.java
+++ src/main/java/com/dualagenda/service/EventService.java
@@ -1,7 +1,9 @@
 package com.dualagenda.service;
 
 import com.dualagenda.model.Event;
+
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class EventService {
@@ -14,5 +16,15 @@ public class EventService {
 
     public List<Event> getAllEvents() {
         return events;
+    }
+
+    public boolean removeEvent(Event event) {
+        return events.remove(event);
+    }
+
+    public List<Event> getAllEventsSortedByDate() {
+        List<Event> sorted = new ArrayList<>(events);
+        sorted.sort(Comparator.comparing(Event::getDateTime));
+        return sorted;
     }
 }
